15765ba66280aa4fa7cc22f910afd133570fee77
garrisonhh <garrisonhh@pm.me>
2021-06-06 23:48:32 -0400

    quaternion extensions

--- src/lib/quaternion.c
+++ src/lib/quaternion.c
@@ -3,19 +3,42 @@
 #include "quaternion.h"
 #include "utils.h"
 
+#define DECL_QUAT_BASIC(fn_name, oper) \
+	quat_t fn_name(quat_t a, quat_t b) {\
+		return (quat_t){a.w oper b.w, a.x oper b.x, a.y oper b.y, a.z oper b.z};\
+	}
+
 quat_t quat_from_v3d(v3d v) {
 	return (quat_t){0.0, v.x, v.y, v.z};
 }
 
+DECL_QUAT_BASIC(quat_add, +)
+DECL_QUAT_BASIC(quat_sub, -)
+DECL_QUAT_BASIC(quat_mul, *)
+DECL_QUAT_BASIC(quat_div, /)
+
+double quat_magnitude(quat_t q) {
+	return sqrt(q.w * q.w + q.x * q.x + q.y * q.y + q.z * q.z);
+}
+
+quat_t quat_scale(quat_t q, double scalar) {
+	return (quat_t){q.w * scalar, q.x * scalar, q.y * scalar, q.z * scalar};
+}
+
+quat_t quat_normalize(quat_t q) {
+	return quat_scale(q, 1.0 / quat_magnitude(q));
+}
+
 quat_t quat_rotation(double angle, v3d about) {
 	double sin_a = sin(angle / 2.0);
-
-	return (quat_t){
+	quat_t rot = {
 		.w = cos(angle / 2.0),
 		.x = sin_a * about.x,
-		.y = sin_a * about.y,
-		.z = sin_a * about.z
+		.y = -(sin_a * about.y),
+		.z = -(sin_a * about.z)
 	};
+
+	return quat_normalize(rot);
 }
 
 quat_t quat_inverse(quat_t q) {
@@ -34,7 +57,8 @@ quat_t quat_hamilton_prod(quat_t a, quat_t b) {
 v3d quat_rotate_v3d(v3d v, quat_t rotation) {
 	quat_t p = quat_from_v3d(v);
 
-	p = quat_hamilton_prod(quat_hamilton_prod(rotation, p), quat_inverse(rotation));
+	p = quat_hamilton_prod(rotation, p);
+	p = quat_hamilton_prod(p, quat_inverse(rotation));
 
 	return (v3d){p.x, p.y, p.z};
 }
--- src/lib/quaternion.h
+++ src/lib/quaternion.h
@@ -10,8 +10,17 @@ typedef struct quat_t quat_t;
 
 quat_t quat_from_v3d(v3d);
 
+quat_t quat_add(quat_t, quat_t);
+quat_t quat_sub(quat_t, quat_t);
+quat_t quat_mul(quat_t, quat_t);
+quat_t quat_div(quat_t, quat_t);
+
+double quat_magnitude(quat_t);
+quat_t quat_scale(quat_t, double scalar);
+quat_t quat_normalize(quat_t);
 quat_t quat_rotation(double angle, v3d about);
 quat_t quat_inverse(quat_t);
+
 quat_t quat_hamilton_prod(quat_t, quat_t);
 
 v3d quat_rotate_v3d(v3d, quat_t rotation);
