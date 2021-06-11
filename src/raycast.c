a801dbc41597938a19a1f05f77117d110da46c27
garrisonhh <garrisonhh@pm.me>
2021-06-11 11:41:16 -0400

    FUNCTIONAL BLOCK PLACEMENT OMG!
    fixed that bug - this is truly one of the greatest achievements of
    mankind

--- src/raycast.c
+++ src/raycast.c
@@ -6,21 +6,19 @@
 // TODO skip over empty chunks
 bool raycast_voxels(world_t *world, ray_t ray, v3i *out_block, int *out_axis) {
 	int i;
-	bool P = out_axis != NULL;
 
 	// initialization step
 	double cur_index;
-	v3i loc = v3i_from_v3d(ray.pos);
+	v3i loc, end;
 	v3i step = polarity_of_v3d(ray.dir);
-	v3i end;
 	v3d t_max, t_delta;
 
 	for (i = 0; i < 3; ++i) {
-		if (v3i_IDX(step, i) > 0) {
-			cur_index = ceil(v3d_IDX(ray.pos, i));
-		} else {
+		if (v3i_IDX(step, i) < 0) {
+			--v3d_IDX(ray.pos, i);
 			cur_index = floor(v3d_IDX(ray.pos, i));
-			--v3i_IDX(loc, i);
+		} else {
+			cur_index = ceil(v3d_IDX(ray.pos, i));
 		}
 
 		v3d_IDX(t_max, i) = (cur_index - v3d_IDX(ray.pos, i)) / v3d_IDX(ray.dir, i);
@@ -28,23 +26,14 @@ bool raycast_voxels(world_t *world, ray_t ray, v3i *out_block, int *out_axis) {
 		v3d_IDX(t_max, i) = fabs(v3d_IDX(t_max, i));
 		v3d_IDX(t_delta, i) = fabs(v3d_IDX(t_delta, i));
 
+		v3i_IDX(loc, i) = (int)v3d_IDX(ray.pos, i);
 		v3i_IDX(end, i) = (v3i_IDX(step, i) > 0 ? world->block_size : -1);
-	}
-
-	if (P) {
-		v3i_print("loc", loc);
-		v3i_print("step", step);
-		v3i_print("end", end);
-		v3d_print("t_max", t_max);
-		v3d_print("t_delta", t_delta);
 	}
 
 	// incrementation step
 	int axis;
 
 	do {
-		if (P) v3i_print(NULL, loc);
-
 		// find axis with minimum t_max
 		axis = 0;
 
@@ -52,19 +41,14 @@ bool raycast_voxels(world_t *world, ray_t ray, v3i *out_block, int *out_axis) {
 			if (v3d_IDX(t_max, i) < v3d_IDX(t_max, axis))
 				axis = i;
 
-		// increment on axis
+		// increment loc and t_max on axis
 		v3i_IDX(loc, axis) += v3i_IDX(step, axis);
 
-		if (v3i_IDX(loc, axis) == v3i_IDX(end, axis)) {
-			if (P) printf("out of bounds\n");
-
+		if (v3i_IDX(loc, axis) == v3i_IDX(end, axis))
 			return false;
-		}
 
 		v3d_IDX(t_max, axis) += v3d_IDX(t_delta, axis);
 	} while (world_get(world, loc) == NULL);
-
-	if (P) v3i_print("hit block", loc);
 
 	// block hit, return it
 	if (out_block != NULL)
